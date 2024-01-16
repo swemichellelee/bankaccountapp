78cbafdbd688c568d655fa1405774e22b4c36cd9
mlee041317 <michelleleefit@gmail.com>
2024-01-16 11:45:30 -0800

    Creating list of objects, finalizing project requirements

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -1,10 +1,11 @@
 package bankaccountapp;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankAccountApp {
     public static void main(String[] args) {
-        String file = "/Users/michellelee/desktop/NewBankAccounts.csv";
+        List<Account> accounts = new LinkedList<>();
 
         // Unit Testing --
 //        Checking chkacc1 = new Checking("Taylor Swift", "123456789", 1500);
@@ -14,13 +15,27 @@ public class BankAccountApp {
 //        chkacc1.showInfo();
 
         // Read a CSV File then create new Accounts based on that data
+        String file = "/Users/michellelee/desktop/NewBankAccounts.csv";
         List<String[]> newAccountHolders = utilities.CSV.read(file);
         for (String[] accountHolder : newAccountHolders) {
-            System.out.println("NEW ACCOUNT");
-            System.out.println(accountHolder[0]);
-            System.out.println(accountHolder[1]);
-            System.out.println(accountHolder[2]);
-            System.out.println(accountHolder[3]);
+            String name = accountHolder[0];
+            String sSN = accountHolder[1];
+            String accountType = accountHolder[2];
+            double initDeposit = Double.parseDouble(accountHolder[3]);
+            System.out.println(name + " " + sSN + " " + accountType + " $" + initDeposit);
+            if (accountType.equals("Savings")) {
+                accounts.add(new Savings(name, sSN, initDeposit));
+            } else if (accountType.equals("Checking"))
+            {
+                accounts.add(new Checking(name, sSN, initDeposit));
+            } else
+            {
+                System.out.println("ERROR READING ACCOUNT TYPE");
+            }
+        }
+        for (Account acc : accounts) {
+            System.out.println("\n*************************");
+            acc.showInfo();
         }
     }
 }
